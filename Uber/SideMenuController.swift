b693441258b7320e4fa8958fdc6352dd406a2c22
Johnny Perdomo <johnnyperdomo7@gmail.com>
2018-06-25 15:41:27 -0400

    create SideMenu UI

---
+++ Uber/SideMenuController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Uber
+//
+//  Created by Johnny Perdomo on 6/24/18.
+//  Copyright © 2018 Johnny Perdomo. All rights reserved.
+//
+
+import UIKit
+import SideMenu
+
+class SideMenuController: UIViewController {
+
+    let sideMenu = SideMenuManager.default
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var homeLocationLbl: UILabel!
+    @IBOutlet weak var workLocationLbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sideMenuCustom()
+        imageView.layer.cornerRadius = imageView.frame.height / 2
+    }
+    
+    
+    func sideMenuCustom() {
+        sideMenu.menuFadeStatusBar = false
+    }
+    
+    
+    @IBAction func homeLocationBtnPressed(_ sender: Any) {
+        print("home pressed")
+    }
+    
+    @IBAction func workLocationBtnPressed(_ sender: Any) {
+        print("work pressed")
+    }
+    
+}
+
