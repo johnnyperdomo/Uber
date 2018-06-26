37501188d3a6ac0b34e51d911923f42ba668d021
Johnny Perdomo <johnnyperdomo7@gmail.com>
2018-06-25 20:10:38 -0400

    segment Control Change / pick different car types, for different prices

--- Uber/Controller/MainVC.swift
+++ Uber/Controller/MainVC.swift
@@ -11,6 +11,7 @@ import MapKit
 
 class MainVC: UIViewController {
 
+    var carType = CarType.uberPool
     
     @IBOutlet weak var mapKitView: MKMapView!
     @IBOutlet weak var destinationView: UIView!
@@ -21,7 +22,7 @@ class MainVC: UIViewController {
     @IBOutlet weak var maxSizeLbl: UILabel!
     @IBOutlet weak var requestRideBtn: UIButton!
     
- 
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -30,12 +31,35 @@ class MainVC: UIViewController {
         segmentControl.layer.cornerRadius = 20
     }
 
- 
+    func configureCarType(etaLbl: String, fareLbl: String, maxSizeLabel: String) {
+        self.etaLbl.text = etaLbl
+        self.fareLbl.text = fareLbl
+        self.maxSizeLbl.text = maxSizeLabel
+    }
+    
+    
+    
     @IBAction func enterLocationBtnPressed(_ sender: Any) {
         print("Enter Location")
     }
     @IBAction func requestRideBtnPressed(_ sender: Any) {
         print("Request Ride")
+    }
+    
+    @IBAction func carTypePicked(_ sender: Any) {
+        if segmentControl.selectedSegmentIndex == 0 {
+            configureCarType(etaLbl: "2 Min", fareLbl: "$3.00 +", maxSizeLabel: "1 Person")
+            carType = .uberPool
+
+        } else if segmentControl.selectedSegmentIndex == 1 {
+            configureCarType(etaLbl: "3 Min", fareLbl: "$8.00 +", maxSizeLabel: "2 People")
+            carType = .uberX
+            
+        } else if segmentControl.selectedSegmentIndex == 2 {
+            configureCarType(etaLbl: "5 Min", fareLbl: "$15.00 +", maxSizeLabel: "4 People")
+            carType = .uberLux
+
+        }
     }
     
 }
