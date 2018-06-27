fbbe0cf1a81de50794eea04842997438b357298d
Johnny Perdomo <johnnyperdomo7@gmail.com>
2018-06-27 18:36:40 -0400

    can search for addresses and related queries will display in a tableView

--- Uber/Controller/SetHomeVC.swift
+++ Uber/Controller/SetHomeVC.swift
@@ -7,15 +7,22 @@
 //
 
 import UIKit
+import MapKit
 
-class SetHomeVC: UIViewController {
+class SetHomeVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var searchTableView: UITableView!
     
+    var searchCompleter = MKLocalSearchCompleter()
+    var searchResults = [MKLocalSearchCompletion]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        searchBar.delegate = self
+        searchCompleter.delegate = self
+        searchTableView.delegate = self
+        searchTableView.dataSource = self
     }
 
     
@@ -23,5 +30,56 @@ class SetHomeVC: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
-   
+    
+    @IBAction func setBtnPressed(_ sender: Any) {
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return searchResults.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = searchTableView.dequeueReusableCell(withIdentifier: "searchCompletionCell", for: indexPath) as? SearchCompletionCell else {return UITableViewCell()}
+        let searchResult = searchResults[indexPath.row]
+        
+        cell.textLbl.text = searchResult.title
+        cell.detailTxtLbl.text = searchResult.subtitle
+        
+        return cell
+        
+    }
+    
+    
+    
 }
+
+extension SetHomeVC: MKLocalSearchCompleterDelegate {
+    
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        searchResults = completer.results
+        searchTableView.reloadData()
+        print("results successful")
+    }
+}
+
+extension SetHomeVC: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if !searchText.isEmpty {
+            searchCompleter.queryFragment = searchBar.text!
+        }
+    }
+}
+
+
+
+
+
+
---
+++ Uber/Views/SearchCompletionCell.swift
@@ -0,0 +1,23 @@
+//
+//  SearchCompletionCell.swift
+//  Uber
+//
+//  Created by Johnny Perdomo on 6/27/18.
+//  Copyright © 2018 Johnny Perdomo. All rights reserved.
+//
+
+import UIKit
+
+class SearchCompletionCell: UITableViewCell {
+
+    @IBOutlet weak var textLbl: UILabel!
+    @IBOutlet weak var detailTxtLbl: UILabel!
+    
+    
+    func configureCell(textLabel: String, detailTxtLabel: String) {
+        self.textLbl.text = textLabel
+        self.detailTxtLbl.text = detailTxtLabel
+        
+    }
+    
+}
