41a04a93e05901d982dd83f8c4b02dcbd26ff0de
Johnny Perdomo <johnnyperdomo7@gmail.com>
2018-07-06 09:35:46 -0400

    create recentTripsVC and configure tableView cells

---
+++ Uber/Controller/RecentTripsVC.swift
@@ -0,0 +1,19 @@
+//
+//  RecentTripsVC.swift
+//  Uber
+//
+//  Created by Johnny Perdomo on 7/6/18.
+//  Copyright © 2018 Johnny Perdomo. All rights reserved.
+//
+
+import UIKit
+
+class RecentTripsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ Uber/Views/TripsCell.swift
@@ -0,0 +1,33 @@
+//
+//  TripsCell.swift
+//  Uber
+//
+//  Created by Johnny Perdomo on 7/6/18.
+//  Copyright © 2018 Johnny Perdomo. All rights reserved.
+//
+
+import UIKit
+
+class TripsCell: UITableViewCell {
+
+    @IBOutlet weak var carTypeLbl: UILabel!
+    @IBOutlet weak var dateTimeLbl: UILabel!
+    @IBOutlet weak var pickUpLbl: UILabel!
+    @IBOutlet weak var destinationLbl: UILabel!
+    @IBOutlet weak var tripTimeLbl: UILabel!
+    @IBOutlet weak var ridersLbl: UILabel!
+    @IBOutlet weak var priceLbl: UILabel!
+    
+
+    func configureCell(carType: String, dateTime: String, pickUp: String, destination: String, tripTime: String, riders: String, price: String) {
+        self.carTypeLbl.text = carType
+        self.dateTimeLbl.text = dateTime
+        self.pickUpLbl.text = pickUp
+        self.destinationLbl.text = destination
+        self.tripTimeLbl.text = tripTime
+        self.ridersLbl.text = riders
+        self.priceLbl.text = price
+    }
+    
+    
+}
